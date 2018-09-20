c2a04f8689ad176b96e6c0630f2ad4a24c037f18
yylcode <1187148000@qq.com>
2018-09-20 17:38:28 +0800

    header-finish

---
+++ src/pages/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="content"> 
+		<button>返回</button>
+		通讯录
+		<button>主页</button>
+
+	</div>
+</template>
+<script>
+	export default{
+		name:'Header'
+	}
+</script>
+<style>
+	.content{
+		height: 40px;
+		background: grey;
+		line-height: 40px;
+		text-align: center;
+		font-size: 20px;
+		color: white;
+	}
+	.content button{
+		height: 38px;
+		position: fixed;
+		padding: 5px;
+		top: 1px;
+		font-weight: bold;
+		color: black;
+		background: white;
+	}
+	.content button:nth-child(1){
+		left: 1px;
+	}
+	.content button:nth-child(2){
+		right: 1px;
+	}
+</style>
