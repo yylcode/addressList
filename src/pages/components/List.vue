b4d7478a00e167a3034d47e932faf81e834aad6d
yylcode <1187148000@qq.com>
2018-09-24 13:25:25 +0800

    new

--- src/pages/components/List.vue
+++ src/pages/components/List.vue
@@ -1,6 +1,6 @@
 <template>
 	<div id="list">
-		<ul class="list_user">
+		<ul class="list_user" ref="listUser">
 			<li v-for="item in userdata">
 				<p>{{item.index}}</p>
 				<ul>
@@ -9,9 +9,8 @@
 			</li>
 			 
 		</ul>
-		<ul class="list_index">
-			<li v-for="item in userdata">{{item.index}}</li>
-			
+		<ul class="list_index" ref="listIndex">
+			<li @touchstart="setScroll" v-for="item in userdata">{{item.index}}</li>
 		</ul>
 	</div>
 </template>
@@ -25,7 +24,19 @@
 					return [];
 				}
 			}
+		},
+		methods:{
+			
+			setScroll:function(ev){
+				var aP=this.$refs.listUser.getElementsByTagName('p');
+				for(var i=0;i<aP.length;i++){
+					if (aP[i].innerHTML==ev.target.innerHTML) {
+						document.documentElement.scrollTop=aP[i].offsetTop;
+					}
+				}
+			}
 		}
+		
 	}
 </script>
 <style type="text/css" media="screen">
@@ -47,12 +58,13 @@
 	.list_index{
 		position: fixed;
 		right: 10px;
-		top: 16%;
-		font-size: 12px;
+		top: 12%;
+		font-size: 14px;
+		font-family: "宋体";
+		padding: 5px;
 		list-style: none;
-		padding: 4px;
-		line-height: 19px;
-		color: black;
-		opacity: 0.6;
+	}
+	.list_index li{
+		margin-top: 5px;
 	}
 </style>
